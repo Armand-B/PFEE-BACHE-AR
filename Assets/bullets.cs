c3192f0b0cb26cf0b8cc737ebb4f55d28f3aaf7b
Armand <armand.briquet@gmail.com>
2020-01-04 17:45:35 +0100

    Ned can shoot

---
+++ Assets/bullets.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullets : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.velocity = new Vector2(0,speed);
+    }
+
+}
---
+++ Assets/shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public GameObject Bullet_Emitter;
+
+    public GameObject Bullet;
+
+    public float Bullet_Forward_Force;
+
+
+    public void Shoot()
+    {
+        GameObject Temporary_Bullet_Handler;
+        Temporary_Bullet_Handler = Instantiate(Bullet,Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+       
+        Destroy(Temporary_Bullet_Handler, 5.0f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
