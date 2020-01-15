8d4144e801cf098244481293c8882a6fab873fc1
Armand <armand.briquet@gmail.com>
2020-01-16 00:33:58 +0100

    Jeu 3 : Done

---
+++ Assets/revealPiece.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class revealPiece : MonoBehaviour
+{
+    public string fragmentName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.transform.Find(fragmentName))
+        {
+            this.gameObject.SetActive(false);
+            other.transform.Find(fragmentName).gameObject.SetActive(false);
+        }
+    }
+}
