585c4c01671f2dfa1c1d6294bc50e979e5754cca
Armand <armand.briquet@gmail.com>
2020-01-16 01:23:10 +0100

    fixed jeu 2

--- Assets/asteroid.cs
+++ Assets/asteroid.cs
@@ -10,11 +10,18 @@ public class asteroid : MonoBehaviour
     private static int index = 0;
 
     // je dois pas etre au bon endroit , je ne peux pas acceder au 3dtext depuis une instance de prefab
-    private string textCode = "ReactDOM.render'(<h1>Hello, world!</h1>,document.getElementById('root'));";
+    private List<string> textCode = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
+        textCode.Add("ReactDOM");
+        textCode.Add(".render'(\n");
+        textCode.Add("<h1>Hello,");
+        textCode.Add("world!</h1>,\n");
+        textCode.Add("document");
+        textCode.Add(".getElementById");
+        textCode.Add("('root')\n);");
         rb = this.GetComponent<Rigidbody>();
         rb.velocity = new Vector2(0,-speed);
     }
@@ -25,17 +32,19 @@ public class asteroid : MonoBehaviour
         if(transform.position.y < -0.400)
         {
             Destroy(this.gameObject);
-        }  
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
         text = GameObject.Find("Codetxt");
         if(other.tag == "bullet")
         {
-            if(index == 17 || index == 40 || index == 70)
-                text.GetComponent<TextMesh>().text += "\n";
-            text.GetComponent<TextMesh>().text += textCode[index];
-            index++;
+            if(index < 7)
+            {
+                text.GetComponent<TextMesh>().text += textCode[index];
+                index++;
+
+            }
             Destroy(this.gameObject);
         }  
     }
