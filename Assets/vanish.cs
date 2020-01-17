5c056f4cd40c45e4ea69766e1f8275d3ac0c8d76
Armand <armand.briquet@gmail.com>
2020-01-17 21:52:53 +0100

    restart button done

---
+++ Assets/vanish.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vanish : MonoBehaviour
+{
+    public float fadeSpeed = 1.0f;
+    private int entered = 0;
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.name == "ned")
+        {
+            entered = 1;
+        }    
+    }
+
+    private void Update() 
+    {
+        if(entered == 1)
+        {
+            Color color = this.GetComponent<MeshRenderer>().material.color ;
+            color.a -= fadeSpeed;
+            this.GetComponent<MeshRenderer>().material.color = color ;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+         if(other.name == "ned")
+        {
+            entered = 0;
+        }  
+    }
+}
